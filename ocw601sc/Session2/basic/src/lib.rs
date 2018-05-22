c0c09d379bc3d403672abc3126930fd04e9032e4
Seb Ospina <kraige@gmail.com>
2018-05-22 21:22:04 +0200

    Fixing generic output for get_next_values

--- ocw601sc/Session2/basic/src/lib.rs
+++ ocw601sc/Session2/basic/src/lib.rs
@@ -1,8 +1,9 @@
 pub trait StateMachine<StateType=Self> {
   type State;
+  type OutputType;
   fn start(&mut self);
   fn step(&mut self, inp: StateType);
-  fn get_next_values<T>(&self, state: StateType, inp: StateType) -> (Self::State,T);
+  fn get_next_values(&self, state: StateType, inp: StateType) -> (Self::State,Self::OutputType);
 }
 pub struct Accumulator {
   pub start_state: i64,
@@ -10,14 +11,15 @@ pub struct Accumulator {
 }
 impl StateMachine<i64> for Accumulator {
   type State = i64;
+  type OutputType = i64;
   fn start(&mut self) {
     self.state = self.start_state;
   }
   fn step(&mut self, inp: i64) {
-    let mut outp:(i64,i64) = self.get_next_values(self.state, inp);
+    let outp:(i64,i64) = self.get_next_values(self.state, inp);
     self.state = outp.0;
   }
-  fn get_next_values<i64>(&self, state: i64, inp: i64) -> (i64,i64) {
+  fn get_next_values(&self, state: i64, inp: i64) -> (i64,i64) {
     (inp + state, inp + state)
   }
 }
