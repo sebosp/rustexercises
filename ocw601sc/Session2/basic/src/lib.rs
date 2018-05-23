08bd8025cbb7aa38d5c454495c242e20df32d6dc
Seb Ospina <kraige@gmail.com>
2018-05-23 21:53:43 +0200

    Adding Transduce functions

--- ocw601sc/Session2/basic/src/lib.rs
+++ ocw601sc/Session2/basic/src/lib.rs
@@ -1,9 +1,11 @@
 pub trait StateMachine<StateType=Self> {
   type State;
   type OutputType;
+  fn new(initial_value: StateType) -> Self;
   fn start(&mut self);
-  fn step(&mut self, inp: StateType);
+  fn step(&mut self, inp: StateType) -> Self::OutputType;
   fn get_next_values(&self, state: StateType, inp: StateType) -> (Self::State,Self::OutputType);
+  fn transduce(&mut self, inp: Vec<StateType>, verbose: bool, compact: bool) -> Vec<Result<Self::OutputType, String>>;
 }
 pub struct Accumulator {
   pub start_state: i64,
@@ -12,14 +14,35 @@ pub struct Accumulator {
 impl StateMachine<i64> for Accumulator {
   type State = i64;
   type OutputType = i64;
+  fn new(initial_value: i64) -> Self{
+    Accumulator {
+      start_state: initial_value,
+      state: initial_value
+    }
+  }
   fn start(&mut self) {
     self.state = self.start_state;
   }
-  fn step(&mut self, inp: i64) {
+  fn step(&mut self, inp: i64) -> i64 {
     let outp:(i64,i64) = self.get_next_values(self.state, inp);
     self.state = outp.0;
+    outp.1
   }
   fn get_next_values(&self, state: i64, inp: i64) -> (i64,i64) {
     (inp + state, inp + state)
+  }
+  fn transduce(&mut self, inp: Vec<i64>, verbose: bool, _: bool) -> Vec<Result<i64,String>> {
+    let mut res: Vec<Result<i64, String>> = Vec::new();
+    if verbose {
+      println!("Start state: {}", self.state);
+    }
+    for cur_inp in inp {
+      let cur_out = self.step(cur_inp);
+      if verbose {
+        println!("In: {} Out: {} Next State: {}", cur_inp, cur_out, self.state);
+      }
+      res.push(Ok(self.state));
+    }
+    res
   }
 }
--- ocw601sc/Session2/basic/tests/basic.rs
+++ ocw601sc/Session2/basic/tests/basic.rs
@@ -4,12 +4,12 @@ mod tests {
   use sm_basics::*;
   #[test]
   fn test_accumulator() {
-    let mut test = Accumulator {
-      state: 0i64,
-      start_state: 0i64
-    };
+    let mut test = Accumulator::new(0);
     test.start();
     test.step(10i64);
     assert_eq!(test.state,10i64);
+    let mut test_transduce = Accumulator::new(0);
+    let transduce_res: Vec<Result<i64,String>> = test_transduce.transduce(vec![100i64, -3i64, 4i64, -123i64, 10i64],true, true);
+    assert_eq!(transduce_res, vec![Ok(100i64), Ok(97i64), Ok(101i64), Ok(-22i64), Ok(-12i64)]);
   }
 }
