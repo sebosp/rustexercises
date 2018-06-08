d7bf1aee6a47cc1a1ab9becdfb5d2ec4c10b8ad4
Seb Ospina <kraige@gmail.com>
2018-06-08 23:24:44 +0200

    Transformed into generics sumlast3

--- ocw601sc/Session2/basic/src/sumlast3.rs
+++ ocw601sc/Session2/basic/src/sumlast3.rs
@@ -3,16 +3,23 @@
 //! anything (a dictionary, an array, a list, tuple); it is important to be sure
 //! that the `get_next_values` method does not make direct changes to components
 //! of the state, instead returning a new copy of the state with appropriate changes.
-pub struct SumLast3 {
-  pub state: (i64,i64),
+extern crate num_traits;
+use num_traits::*;
+use std::fmt::Display;
+pub struct SumLast3<T>
+where T: Num + Display + Clone + Copy
+{
+  pub state: (T,T),
 }
-impl super::StateMachine for SumLast3 {
+impl<T> super::StateMachine for SumLast3<T>
+where T: Num + Display + Clone + Copy
+{
   /// `StateType`(S) = tuple of numbers
-  type StateType = (i64,i64);
+  type StateType = (T,T);
   /// `InputType`(I) = number
-  type InputType = i64;
+  type InputType = T;
   /// `OutputType`(O) = number
-  type OutputType = i64;
+  type OutputType = T;
   /// `initial_value`(_s0_) is (0,0)
   fn new(initial_value: Self::StateType) -> Self {
     SumLast3 {
@@ -20,7 +27,7 @@ impl super::StateMachine for SumLast3 {
     }
   }
   fn start(&mut self){
-    self.state = (0i64,0i64)
+    self.state = (T::zero(),T::zero())
   }
   fn step(&mut self, inp: &Self::InputType) -> Result<Self::OutputType, String> {
     let outp:(Self::StateType,Self::OutputType) = self.get_next_values(self.state,*inp)?;
@@ -39,5 +46,23 @@ impl super::StateMachine for SumLast3 {
   }
   fn verbose_step(&self,inp: &Self::InputType, outp: &Self::OutputType) -> String {
      format!("In: {} Out: {} Next State: ({},{})", inp, outp, self.state.0, self.state.1)
+  }
+}
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use super::super::*;
+  #[test]
+  fn it_gets_next_values() {
+    let test = SumLast3::new((0i8,1i8));
+    assert_eq!(test.get_next_values((0i8,0i8),1i8),Ok(((0i8,1i8),1i8)));
+    assert_eq!(test.get_next_values((5i8,7i8),3i8),Ok(((7i8,3i8),15i8)));
+    assert_eq!(test.get_next_values((3i8,1i8),5i8),Ok(((1i8,5i8),9i8)));
+  }
+  #[test]
+  fn it_steps() {
+    let mut test = SumLast3::new((0i8,1i8));
+    assert_eq!(test.step(&2i8),Ok(3i8));
+    assert_eq!(test.state,(1i8,2i8));
   }
 }
