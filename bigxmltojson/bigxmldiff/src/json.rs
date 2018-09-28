69ce10644835b6426dfbf404915ce507ebe3f1fa
Seb Ospina <kraige@gmail.com>
2018-09-28 13:21:08 +0200

    Created several utilities for JsonDataType

--- bigxmltojson/bigxmldiff/src/json.rs
+++ bigxmltojson/bigxmldiff/src/json.rs
@@ -27,6 +27,39 @@ impl fmt::Display for JsonData {
   }
 }
 
+impl JsonDataType {
+  /// `new` Creates an Empty JsonDataType by default
+  pub fn new() -> JsonDataType {
+    JsonDataType::Empty
+  }
+  /// `new_atomic_from_string` Creates a JsonDataType String input
+  pub fn new_atomic_from_string(input: String) -> JsonDataType {
+    JsonDataType::Atomic(
+      RefCell::new(input)
+    )
+  }
+  /// `new_array_from_string` Creates a JsonDataType Array from a String input
+  pub fn new_array_from_string(input: String) -> JsonDataType {
+    JsonDataType::Array(
+      RefCell::new(
+        vec![
+          JsonData::new_atomic_from_string(input)
+        ]
+      )
+    )
+  }
+  /// `new_object_from_kv_strinsg` Creates a JsonDataType Object from
+  /// String Key,Value pair.
+  pub fn new_object_from_kv_strings(input_key: String, input_value: String) -> JsonDataType {
+    JsonDataType::Object(
+      RefCell::new(
+        vec![
+          (input_key,JsonData::new_atomic_from_string(input_value))
+        ]
+      )
+    )
+  }
+}
 impl JsonData {
   /// `new` returns a new JsonData structure.
   /// XXX: Should this be Rc<> ?
@@ -37,7 +70,9 @@ impl JsonData {
   /// ```
   pub fn new() -> Self {
     JsonData {
-      data: Rc::new(JsonDataType::Empty),
+      data: Rc::new(
+              JsonDataType::new()
+            ),
       parent: RefCell::new(Weak::new()),
     }
   }
@@ -50,7 +85,9 @@ impl JsonData {
   /// ```
   pub fn new_atomic_from_string(input: String) -> JsonData {
     JsonData {
-      data: Rc::new(JsonDataType::Atomic(RefCell::new(input))),
+      data: Rc::new(
+              JsonDataType::new_atomic_from_string(input)
+            ),
       parent: RefCell::new(Weak::new()),
     }
   }
@@ -59,11 +96,28 @@ impl JsonData {
   /// # Example
   /// ```
   /// let simple_array = JsonData::new_array_from_string("String".to_owned());
-  /// assert_eq!(simple_array.to_string(),"\"[String]\"".to_owned());
+  /// assert_eq!(simple_array.to_string(),"[\"String\"]".to_owned());
   /// ```
   pub fn new_array_from_string(input: String) -> JsonData {
     JsonData {
-      data: Rc::new(JsonDataType::Array(RefCell::new(vec![JsonData::new_atomic_from_string(input)]))),
+      data: Rc::new(
+              JsonDataType::new_array_from_string(input)
+            ),
+      parent: RefCell::new(Weak::new()),
+    }
+  }
+  /// `new_object_from_kv_strings` creates a JsonData of JsonDataType Array for
+  /// an input String.
+  /// # Example
+  /// ```
+  /// let simple_obj = JsonData::new_object_from_kv_strings("String".to_owned());
+  /// assert_eq!(simple_obj.to_string(),"{\"Key\":\"Value\"}".to_owned());
+  /// ```
+  pub fn new_object_from_kv_strings(input_key: String, input_value: String) -> JsonData {
+    JsonData {
+      data: Rc::new(
+              JsonDataType::new_object_from_kv_strings(input_key,input_value)
+            ),
       parent: RefCell::new(Weak::new()),
     }
   }
@@ -83,17 +137,19 @@ impl JsonData {
     )
   }
   /// `obj_wrap` wraps a JsonData into JsonDataType Array
-  pub fn obj_wrap(self, key: String) -> JsonDataType {
-    JsonDataType::Object(
-      RefCell::new(
-        vec![
-          (key,
-            JsonData {
-              data: self.data,
-              parent: RefCell::new(Weak::new()),
-            }
-          )
-        ]
+  pub fn obj_wrap(&self, key: String) -> Rc<JsonDataType> {
+    Rc::new(
+      JsonDataType::Object(
+        RefCell::new(
+          vec![
+            (key,
+              JsonData {
+                data: Rc::clone(&self.data),
+                parent: RefCell::new(Weak::new()),
+              }
+            )
+          ]
+        )
       )
     )
   }
@@ -126,7 +182,10 @@ impl JsonData {
     let mut inside_tag = false;
     let mut cur_tag = String::with_capacity(128);
     let mut cur_tag_content = String::with_capacity(128);
-    let res = JsonData::new();
+    let mut res = JsonData::new();
+    if verbosity > 1 {
+      println!("WORKER[{}] Working on content: {}", worker_id_string, data);
+    }
     for cur_char in data.chars() {
       if cur_char == '<' {
         inside_tag = true;
@@ -138,11 +197,11 @@ impl JsonData {
         }
         if cur_tag.starts_with('/') {
           cur_tag.drain(..1);
-          println!("WORKER[{}] Current tag '{{{}:{}}} '", worker_id_string, cur_tag, cur_tag_content);
-          res.insert_kv(cur_tag.clone(),cur_tag_content.clone());
+          println!("WORKER[{}] Current tag '{{{}:{}}}'", worker_id_string, cur_tag, cur_tag_content);
           if cur_tag_content.len() > 0 {
-            // XXX: Continue
+            res.insert_kv(cur_tag.clone(),cur_tag_content.clone());
           } else {
+            res.insert(cur_tag.clone());
           }
         }
         cur_tag_content.truncate(0);
@@ -162,16 +221,28 @@ impl JsonData {
         }
       }
     }
-    Ok(JsonData::new_atomic_from_string("Unimplemented".to_string()))
+    Ok(res)
   }
   /// `transform_to_atomic` an entry currently Empty variant to an Atomic variant.
   pub fn transform_to_atomic(&self, input: String) -> Result<Rc<JsonDataType>,String> {
     match &*self.data {
       JsonDataType::Empty => {
-        Ok(Rc::new(JsonDataType::Atomic(RefCell::new(input))))
+        Ok(Rc::new(JsonDataType::new_atomic_from_string(input)))
       },
       _ => {
         Err("transform_to_atomic supports only Empty JsonDataType".to_owned())
+      },
+    }
+  }
+  /// `transform_to_object` an entry currently Empty variant to an Object
+  /// variant.
+  pub fn transform_to_object(&self, input_key: String, input_value: String) -> Result<Rc<JsonDataType>,String> {
+    match &*self.data {
+      JsonDataType::Empty => {
+        Ok(Rc::new(JsonDataType::new_object_from_kv_strings(input_key,input_value)))
+      },
+      _ => {
+        Err("transform_to_object supports only Empty JsonDataType".to_owned())
       },
     }
   }
@@ -190,43 +261,69 @@ impl JsonData {
       _ => {},
     }
   }
-  /// `insert` to the current level
+  /// `vivify_path` Creates a path structure if needed and inserts a string
+  /// on it.
+  /// The path comes as a dot separated values.
+  /// If a path doesn't exist, it will create it as a JSON Object.
+  /// If the path exists, it may need to wrap a given JSON Object in to an Array.
+  /// # Example:
+  /// ```
+  /// let mut test_obj = JsonData::vivify_path("root.keys.alpha.id".to_owned(),"1".to_owned());
+  /// assert_eq!(test_obj.to_string(),"{\"root\":{\"keys\":{\"alpha\":\"1\"}}}".to_owned());
+  /// test.obj.JsonData::vivify_path("root.keys.beta.id".to_owned(),"2".to_owned());
+  /// assert_eq!(test_obj.to_string(),"{\"root\":{\"keys\":[{\"alpha\":\"1\"},{\"beta\":\"2\"}]}}".to_owned());
+  /// test.obj.JsonData::vivify_path("root.attrs.beta.name".to_owned(),"BetaAttr".to_owned());
+  /// assert_eq!(test_obj.to_string(),"{\"root\":{\"keys\":[{\"alpha\":\"1\"},{\"beta\":\"2\"}]}}".to_owned());
+  /// ```
+  pub fn vivify_path(&mut self, _path: &String, _input: String) {
+    unimplemented!("XXX");
+  }
+  /// `insert` a string to the current level
   pub fn insert(&mut self, input: String) {
     match &*self.data {
-      JsonDataType::Empty => {
-        self.data = self.transform_to_atomic(input).unwrap();
+      JsonDataType::Object(_) => {
+        self.data = self.obj_wrap(input);
+        //self.push_value(input);
+        //    res.data = res.obj_wrap(cur_tag.clone());
+        //panic!("Unsupported type for insert, use insert_kv");
+      },
+      JsonDataType::Array(_) => {
+        self.push_value(input);
       },
       JsonDataType::Atomic(_) => {
         // XXX: How does one decide if something is to be transformed to Array or to Object?
         self.data = self.array_wrap();
         self.push_value(input);
       },
-      JsonDataType::Array(_) => {
-        self.push_value(input);
-      },
-      JsonDataType::Object(_) => {
-        panic!("Unsupported type for insert, use insert_kv");
+      JsonDataType::Empty => {
+        self.data = self.transform_to_atomic(input).unwrap();
       },
     };
   }
   /// `insert_kv` Inserts a Key/Value pair.
-  pub fn insert_kv(&self, input_key: String, input_value: String) {
+  pub fn insert_kv(&mut self, input_key: String, input_value: String) {
     // If the key exists, add the input_value to the key.
-    match &*self.data {
+    let new_data = match &*self.data {
+      JsonDataType::Empty => {
+        Rc::new(JsonDataType::new_object_from_kv_strings(input_key, input_value))
+      },
       JsonDataType::Object(kv_array_ref) => {
         let mut kv_array = kv_array_ref.borrow_mut();
         for (k,v) in kv_array.iter_mut() {
           if *k == input_key {
-            v.push_value(input_value);
+            v.insert(input_value);
             return;
           }
         }
         kv_array.push((input_key, JsonData::new_atomic_from_string(input_value)));
+        Rc::clone(&self.data)
       },
       _ => {
-        panic!("Unsupported type for insert_kv, use insert");
+        println!("Unsupported type for insert_kv, use insert");
+        Rc::clone(&self.data)
       },
-    }
+    };
+    self.data = new_data;
   }
 }
 #[cfg(test)]
@@ -234,7 +331,7 @@ mod tests {
   use super::*;
   #[test]
   fn it_transforms_from_xml_string() {
-    let verbosity = 4i8;
+    let verbosity = 3i8;
     assert_eq!(
       JsonData::from_xml_string(&"<div><li>1</li></div>".to_owned(),&"TEST".to_owned(),verbosity).unwrap().to_string(),
       "{\"div\":{\"li\":\"1\"}}".to_owned()
@@ -249,7 +346,7 @@ mod tests {
     );
     assert_eq!(
       JsonData::from_xml_string(&"<body><div><li>1</li></div><div><li>2</li></div></body>".to_owned(),&"TEST".to_owned(),verbosity).unwrap().to_string(),
-      "{\"items\":{\"div\":[{\"li\":\"1\"},{\"li\":\"2\"}]}}".to_owned()
+      "{\"body\":{\"div\":[{\"li\":\"1\"},{\"li\":\"2\"}]}}".to_owned()
     );
     assert_eq!(
       JsonData::from_xml_string(&"<div><li>1</li><lt>a</lt><li>2</li></div>".to_owned(),&"TEST".to_owned(),verbosity).unwrap().to_string(),
@@ -367,16 +464,7 @@ mod tests {
     let mut simple_array2 = simple_string;
     simple_array2.insert("String2".to_owned());
     assert_eq!(simple_array2.to_string(),"[\"String1\",\"String2\"]".to_owned());
-    let mut simple_obj = JsonData{
-      data: Rc::new(
-        JsonDataType::Object(
-          RefCell::new(
-            vec![("Key".to_owned(), JsonData::new_atomic_from_string("Value1".to_owned()))]
-          )
-        )
-      ),
-      parent: RefCell::new(Weak::new()),
-    };
+    let mut simple_obj = JsonData::new_object_from_kv_strings("Key".to_owned(),"Value1".to_owned());
     simple_obj.insert_kv("Key".to_owned(),"Value2".to_owned());
     assert_eq!(simple_obj.to_string(),"{\"Key\":[\"Value1\",\"Value2\"]}".to_owned());
     let mut multi_kv = JsonData{ data: Rc::new(
@@ -390,7 +478,7 @@ mod tests {
       )),
       parent: RefCell::new(Weak::new()),
     };
-    multi_kv.insert("Value2.1".to_owned());
+    multi_kv.insert_kv("Key2".to_owned(),"Value2.1".to_owned());
     assert_eq!(multi_kv.to_string(),"{\"Key1\":\"Value1\",\"Key2\":[\"Value2.0\",\"Value2.1\"]}".to_owned());
     let mut array_contains_objs = JsonData{
       data: Rc::new(
