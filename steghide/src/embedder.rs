34536c095e90b39cc6b664898a047574e3b1a24b
Seb Ospina <kraige@gmail.com>
2018-12-20 19:30:00 +0100

    Starting to read binary file

--- steghide/src/embedder.rs
+++ steghide/src/embedder.rs
@@ -1,19 +1,35 @@
 use super::OptionalFile;
-pub struct Embedder{}
+use std::fs::File;
+use std::io::Read;
+use std::io::{self, Read};
+use std::io::prelude::*;
+pub struct Embedder{
+    embed_file_contents: Vec<u8>,
+}
 
 impl Embedder{
-    pub fn new(request: super::StegHideRequest) -> Result<Embedder,String> {
-        match request.embedfile {
-            OptionalFile::None => Err("Missing Embed file".to_string()),
+    pub fn new(request: &super::StegHideRequest) -> Result<Embedder,String> {
+        let mut buffer = Vec::new();
+        match &request.embedfile {
+            OptionalFile::None => {
+                error!("Embedder new() needs a data source to work in");
+                return Err("Missing Embed file".to_string());
+            },
             OptionalFile::Stdin => {
-                    info!("reading secret data from standard input...");
-                    Ok(Embedder{})
+                info!("reading secret data from standard input...");
+                io::stdin().read_to_string(&mut buffer).unwrap(); // XXX: Remove unwrap, move to binary_io
             },
             OptionalFile::Some(filename) => {
                 info!("reading secret file {}",filename);
-                Ok(Embedder{})
+                let mut f = File::open(filename).unwrap(); // XXX: Remove unwrap, move to binary_io
+                f.read_to_end(&mut buffer).unwrap();       // XXX: Remove unwrap, move to binary_io
             }
-        }
+        };
+        Ok(Embedder{
+            embed_file_contents: buffer
+        })
+        // - Create vec of bytes
+        // - Use binary_io read the bytes of the file/stdin into this vec
     }
     pub fn run(self) -> Result<String, String> {
         Ok(String::from("embedder::new() is not implemented"))
