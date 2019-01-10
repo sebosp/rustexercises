9911ce6976dd3f13ffc52ef95ef83757db986ca2
Seb Ospina <kraige@gmail.com>
2019-01-10 18:54:21 +0100

    Init of EmbedUtils, for embedding and extracting data

--- steghide/src/embedder.rs
+++ steghide/src/embedder.rs
@@ -1,43 +1,40 @@
 use super::binary_io::BinaryIO;
+use super::embed_utils::*;
 /// `Embedder` runs the Embedding operations
 pub struct Embedder{
     embed_file_contents: Option<Vec<u8>>,
-    enc_algo: super::encryption_algorithm::EncryptionAlgorithm,
-    enc_mode: super::encryption_mode::EncryptionMode,
-    compression_level: u8,
-    nochecksum: bool,
-    embed_name: bool,
-    embedfile: super::OptionalFile,
+    utils: EmbedUtils,
 }
 
 impl Embedder{
     pub fn new(request: &super::StegHideRequest) -> Result<Embedder,String> {
         let buffer = BinaryIO::new(&request.embedfile, "read")?
             .read();
-        Ok(Embedder{
-            embed_file_contents: buffer,
-            enc_algo: request.enc_algo,
-            enc_mode: request.enc_mode,
-            compression_level: request.compression_level,
-            nochecksum: request.nochecksum,
-            embed_name: request.embed_name,
-            embedfile: request.embedfile,
-        })
-    }
-    pub fn run(self) -> Result<String, String> {
-        // create bitstring to be embedded
-        let mut embed_filename:String; 
-        if self.embed_name {
-            embed_filename = match self.embedfile {
+        let mut embed_filename = "".to_string();
+        if request.embed_name {
+            embed_filename = match &request.embedfile {
                 super::OptionalFile::Some(fname) => fname.clone(),
                 _ => "".to_string(),
             };
         }
-        let embed_data = super::EmbedUtilsBuilder::new(
-                super::EmbedUtils::Mode::Embed,
-                self.request.Passphrase,
-            );
-
+        let utils = super::embed_utils::EmbedUtilsBuilder::default()
+            .passphrase(request.passphrase.clone())
+            .filename(embed_filename.clone())
+            .enc_algo(request.enc_algo.clone())
+            .enc_mode(request.enc_mode)
+            .compression_level(request.compression_level)
+            .nochecksum(request.nochecksum)
+            .embed_name(request.embed_name)
+            .embedfile(request.embedfile.clone())
+            .build()?;
+        Ok(Embedder{
+            embed_file_contents: buffer,
+            utils: utils,
+        })
+    }
+    pub fn run(self) -> Result<String, String> {
+        // create bitstring to be embedded
+	    self.utils.get_bit_string() ;
         Ok(String::from("embedder::run() is not implemented"))
     }
 }
