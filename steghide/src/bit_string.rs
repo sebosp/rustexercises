a5ea0db3543d48f623bea2156ee595e1e4bbeb63
Seb Ospina <kraige@gmail.com>
2019-01-16 18:31:42 +0100

    Adedd prefix and defaults for BitString

--- steghide/src/bit_string.rs
+++ steghide/src/bit_string.rs
@@ -1,9 +1,12 @@
 #[derive(Default, Builder, Clone)]
+#[builder(setter(prefix = "with"))]
 pub struct BitString {
     /// the number of bits in Data
+    #[builder(default = "0")]
     length: u32,
     /// the arity that will be used for getLength/getNAry/appendNAry
     /// Arity is also seen as unsigned char which seems to be u8
+    #[builder(default = "2")]
     arity: u8,
     /// the number of Bits per n-ary digit (where n is Arity)
     arity_nbits: u16,
@@ -20,4 +23,11 @@ impl BitString{
             data: vec![]
         }
     }
+    pub fn append(&mut self, bit: bool) {
+        if self.length % 8 == 0 { 
+                self.data.push(0);
+        }
+    }
+    pub fn bit_pos(n) (n % 8)
+#define BYTEPOS(n) (n / 8)
 }
