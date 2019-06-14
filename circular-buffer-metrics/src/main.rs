4b1c48e083912c07772cb8ecf1709f046cbb91cc
Seb Ospina <kraige@gmail.com>
2019-06-14 19:28:04 +0200

    Starting to iterate over config.charts to provide the tokio core

--- circular-buffer-metrics/src/main.rs
+++ circular-buffer-metrics/src/main.rs
@@ -9,7 +9,15 @@ fn main() {
     let mut core = Core::new().unwrap();
     let core_handle = &core.handle();
     let config_location = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/charts.yml"));
-    match circular_buffer_metrics::config::Config::read_config(&config_location) {
+    let mut config_res = circular_buffer_metrics::config::Config::read_config(&config_location);
+    let mut config = match config_res {
+        Err(err) => {
+            error!(
+                "Unable to load config from file: {:?}: {}",
+                config_location, err
+            );
+            return;
+        }
         Ok(ref mut config) => {
             info!("Loaded config from file: {:?}", config_location);
             for chart in &mut config.charts {
@@ -24,13 +32,28 @@ fn main() {
                     }
                 }
             }
-        }
-        Err(err) => {
-            error!(
-                "Unable to load config from file: {:?}: {}",
-                config_location, err
-            );
+            config
         }
     };
+    for chart in &mut config.charts {
+        debug!("Loading chart series {}", chart.name);
+        for series in &mut chart.sources {
+            if let circular_buffer_metrics::TimeSeriesSource::PrometheusTimeSeries(ref mut prom) =
+                series
+            {
+                let chart_data = core.run(prom.get_from_prometheus());
+                if let Ok(Some(prom_response)) = chart_data {
+                    match prom.load_prometheus_response(prom_response) {
+                        Ok(num_records) => {
+                            info!("Loaded {} records", num_records);
+                        }
+                        Err(err) => {
+                            error!("Error loading prometheus response: {}", err);
+                        }
+                    }
+                }
+            }
+        }
+    }
     info!("Exiting.");
 }
